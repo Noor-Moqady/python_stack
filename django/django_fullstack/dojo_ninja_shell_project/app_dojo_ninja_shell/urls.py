8eb687d7a707880939ab60dc6c6a1620d46ce77b
noor-moq <noor.moqady@gmail.com>
2023-11-21 01:01:35 +0200

    one to many relation with templates

--- django/django_fullstack/dojo_ninja_shell_project/app_dojo_ninja_shell/urls.py
+++ django/django_fullstack/dojo_ninja_shell_project/app_dojo_ninja_shell/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from.import views
 urlpatterns = [
-    path('', views.method1),
+    path('', views.handel_form),
+    path('adddojo', views.add_dojo),
+    path('addninja', views.add_ninja)
 ]
--- django/django_fullstack/dojo_ninja_shell_project/app_dojo_ninja_shell/views.py
+++ django/django_fullstack/dojo_ninja_shell_project/app_dojo_ninja_shell/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from.models import *
 
-def method1(request):
-    return HttpResponse("hiiiiiii dojo_ninjs_shell_project")
+def handel_form(request):
+    context = {
+       'alldojos': Dojo.objects.all(),
+       'allninjas':Ninja.objects.all()
+    }
+    return render(request, "dojo_ninja.html",context)
+def add_dojo(request):
+    Dojo.objects.create(name=request.POST['dojoname'], city=request.POST['city'], state=request.POST['state'])
+    return redirect('/')
+
+def add_ninja(request):
+    Ninja.objects.create(first_name=request.POST['fname'], last_name=request.POST['lname'], dojo=Dojo.objects.get(id=request.POST['dojoselesct']))
+    return redirect('/')
