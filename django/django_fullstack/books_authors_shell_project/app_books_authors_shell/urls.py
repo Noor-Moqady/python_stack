a80d288a8af465aea0d5a3b258ed642a6e9d9dbf
noor-moq <noor.moqady@gmail.com>
2023-11-21 14:49:57 +0200

    many to many relations

--- django/django_fullstack/books_authors_shell_project/app_books_authors_shell/urls.py
+++ django/django_fullstack/books_authors_shell_project/app_books_authors_shell/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
 from.import views
 urlpatterns = [
-    path('', views.method1)
+     # PATHS FOR BOOKS PAGE
+    path('', views.handel_form),
+    path('addbooks',views.add_book),
+    path('books/<int:id>', views.specific_book),
+    path('addauthor_to_book/<int:id2>', views.addauthor_to_book),
+    # PATHS FOR AUTHORS PAGE
+    path('authors',views.handel_authorform),
+    path('addauthors',views.add_author),
+    path('authors/<int:id3>', views.specific_author),
+    path('addbook_to_author/<int:id4>', views.addbook_to_author),
 ]
--- django/django_fullstack/books_authors_shell_project/app_books_authors_shell/views.py
+++ django/django_fullstack/books_authors_shell_project/app_books_authors_shell/views.py
@@ -1,6 +1,54 @@
+from django.shortcuts import render, HttpResponse, redirect
+from.models import *
 
 
-from django.shortcuts import render, HttpResponse
+# FOR BOOK PAGES>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+def handel_form(request):
+    context= {
+       'allbooks': Book.objects.all()
+    }
+    return render(request, "books_authors.html", context)
 
-def method1(request):
-    return HttpResponse("hiiiiiii from books - authors - shell project")
+def add_book(request):
+    Book.objects.create(title=request.POST['title'], desc=request.POST['desc'])
+    return redirect('/')
+
+def specific_book(request, id):
+    context={
+        'specific_book':Book.objects.get(id=int(id)),
+        'allauthors': Author.objects.all()
+    }
+    return render(request, "specific_book.html", context)
+
+def addauthor_to_book(request,id2):
+    specific_book=Book.objects.get(id=int(id2))
+    specific_author=Author.objects.get(id=request.POST['authoselesct'])
+    specific_book.authors.add(specific_author)
+    return redirect('/books/'+str(id2))
+    # return redirect(f'/books/{id2}')
+
+
+# FOR AUTHOR PAGES>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+def handel_authorform(request):
+    context= {
+       'allauthors': Author.objects.all()
+    }
+    return render(request, "authors.html", context)  
+
+def add_author(request):
+    Author.objects.create(first_name=request.POST['fname'], last_name=request.POST['lname'], notes=request.POST['note'])
+    return redirect('/authors')
+
+def specific_author(request, id3):
+    context={
+        'specific_author': Author.objects.get(id=int(id3)),
+        'allbooks': Book.objects.all()
+    }
+    return render(request, "specific_author.html", context)
+
+def addbook_to_author(request, id4):
+    specific_book=Book.objects.get(id=request.POST['bookselect'])
+    specific_author=Author.objects.get(id=int(id4))
+    specific_author.books.add(specific_book)
+    return redirect('/authors/'+str(id4))
