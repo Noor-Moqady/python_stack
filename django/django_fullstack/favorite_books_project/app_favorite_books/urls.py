87f45f17e853f51accfe1615c3c196845d794251
noor-moq <noor.moqady@gmail.com>
2023-12-01 17:31:26 +0200

    favorite_book

--- django/django_fullstack/favorite_books_project/app_favorite_books/urls.py
+++ django/django_fullstack/favorite_books_project/app_favorite_books/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('books',views.welcome),
     path('login',views.login),
     path('logout',views.logout),
-    path('addfavoritebooks', views.addfavoritebooks)
-    
+    path('addfavoritebooks', views.addfavoritebooks),
+    path('addfav/<int:id>', views.addfav),
+    path('books/<int:id>', views.update_specific_book),
+    path('removefav/<int:id>', views.removefav),
+    path('delete/<int:id>', views.delete)
 ]
--- django/django_fullstack/favorite_books_project/app_favorite_books/views.py
+++ django/django_fullstack/favorite_books_project/app_favorite_books/views.py
@@ -22,6 +22,8 @@ def registration_form(request):
             user=User.objects.create(first_name=request.POST['fname'], last_name=request.POST['lname'], email=request.POST['email'],password=pw_hash)
             request.session['logged_user_firstname']=user.first_name
             request.session['logged_user_id']=user.id
+            
+            
         return redirect('/books')
     
 
@@ -29,10 +31,11 @@ def registration_form(request):
 def welcome(request):
     if not 'logged_user_id' in request.session:
         messages.error(request,"You have to login first")
-        return redirect('/registration')
+        return redirect('/')
     else:
         context = {
-       'allbooks': Book.objects.all()
+       'allbooks': Book.objects.all(),
+       'favorite_book': User.objects.get(id=request.session['logged_user_id']).books_liked.all()
     }
     return render(request, "welcome.html",context)
     
@@ -50,6 +53,7 @@ def login(request):
             if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
                 request.session['logged_user_firstname']=logged_user.first_name
                 request.session['logged_user_id']=logged_user.id
+        
                 return redirect('/books')
             else:
                 messages.error(request,"Invalid Password")
@@ -65,5 +69,50 @@ def logout(request):
 
 def addfavoritebooks(request):
     favbook=Book.objects.create(title=request.POST['title'], desc=request.POST['desc'],uploaded_by=User.objects.get(id=request.session['logged_user_id']))
+    user_favbook=User.objects.get(id=request.session['logged_user_id'])
+    user_favbook.books_liked.add(favbook)
     return redirect('/books')
-   
+def addfav(request, id):
+    user_favbook=User.objects.get(id=request.session['logged_user_id'])
+    favbook=Book.objects.get(id=int(id))
+    user_favbook.books_liked.add(favbook)
+    return redirect('/books')
+
+def removefav(request, id):
+    user_favbook=User.objects.get(id=request.session['logged_user_id'])
+    favbook=Book.objects.get(id=int(id))
+    user_favbook.books_liked.remove(favbook)
+    return redirect('/books/'+str(id))
+
+def update_specific_book(request, id):
+    if not 'logged_user_id' in request.session:
+        messages.error(request,"You have to login first")
+        return redirect('/')
+    else:
+
+        if request.method == 'GET':
+            context={
+            'specific_book':Book.objects.get(id=int(id)),
+            'logged_user':User.objects.get(id=int(request.session['logged_user_id']))
+        }
+            return render(request,"specific_book.html", context)
+        if request.method == 'POST':
+# ###########VALIDATION PART############################################################################################################################################################################################################################################################
+            errors = Book.objects.basic_validator(request.POST)
+            if len(errors) > 0:
+                for key, value in errors.items():
+                    book=Book.objects.get(id=id)
+                    messages.error(request, value)
+                return redirect('/books/'+str(book.id))
+            else:
+                book=Book.objects.get(id=id)
+                book.title=request.POST['title']
+                book.desc=request.POST['desc']
+                book.save()
+            return redirect('/books/'+str(book.id))
+
+def delete(request, id):
+  specific_book=Book.objects.get(id=int(id))
+  specific_book.delete()
+  return redirect('/books')
+
